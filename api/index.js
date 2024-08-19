7b0d4c74fed2143a46ebcd29dab70f9d6cb76a12
Golemite02 <97048204+Golemite02@users.noreply.github.com>
2024-08-19 13:37:34 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
+import {
+  InteractionType,
+  InteractionResponseType,
+  InteractionResponseFlags,
+  verifyKeyMiddleware,
+} from "discord-interactions";
 
+module.exports = (req, res) => {
+  const { name = 'boy' } = req.query;
+  res.status(200).json({
+    message: `Hello ${name}!`,
+  });
+};
